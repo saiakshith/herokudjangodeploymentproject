f3df03b9bf9272da2728bb2b704460319e48a217
saiakshith <vangarusaiakshith98@gmail.com>
2021-03-20 23:05:27 +0530

    Second commit

--- herokudeployment/settings.py
+++ herokudeployment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o4pzj#2h_iz&s&w#!khbu5b&yrnbfo+bwj^90u_)@61^zio)mb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
