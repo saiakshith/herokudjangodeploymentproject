72c99802c777898bf14a61ac823be0c057780d62
saiakshith <vangarusaiakshith98@gmail.com>
2021-03-21 00:37:54 +0530

    very silly mistake \ ok

--- herokudeployment/settings.py
+++ herokudeployment/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["desolate-garden-40288.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["desolate-garden-40288.herokuapp.com", "localhost"]
 
 
 # Application definition
